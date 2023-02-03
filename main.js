ca876eb5cb64022b3edde62dde09994d00a8cadc
Brandon Bhangari <b6_10_94_furlonge@hotmail.com>
2023-02-03 15:11:10 -0500

    finished solution for mobile and desktop

---
+++ main.js
@@ -0,0 +1,25 @@
+const dice = document.querySelector(".dice-container");
+const number = document.querySelector(".advice-number");
+const advice = document.querySelector(".advice-text");
+
+const getAdvice = async () => {
+  const response = await fetch("https://api.adviceslip.com/advice");
+
+  const data = await response.json();
+
+  //   console.log(data.slip.id);
+
+  //   number.innerHTML = data.slip.id;
+  //   advice.innerHTML = data.slip.advice;
+  return data;
+};
+
+const generateAdvice = async () => {
+  const data = await getAdvice();
+  const id = data.slip.id;
+  const adviceText = data.slip.advice;
+  number.innerHTML = id;
+  advice.innerHTML = adviceText;
+};
+
+dice.addEventListener("click", generateAdvice);
